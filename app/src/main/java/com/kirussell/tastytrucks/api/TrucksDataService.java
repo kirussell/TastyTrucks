45031fc9c681219520f120cb2f3417745e75f70a
russellkim <russellkim@yandex-team.ru>
2016-04-05 01:13:01 +0300

    - interface for food trucks' data api

---
+++ app/src/main/java/com/kirussell/tastytrucks/api/TrucksDataService.java
@@ -0,0 +1,18 @@
+package com.kirussell.tastytrucks.api;
+
+import com.kirussell.tastytrucks.api.data.TruckData;
+
+/**
+ * Created by russellkim on 05/04/16.
+ */
+public interface TrucksDataService {
+
+    /**
+     * Trucks in circle area with center in [latitude, longitude] and radius in meters
+     * @param latitude center of circle area
+     * @param longitude center of circle area
+     * @param distanceInMeters radius of circle area
+     * @return trucks data within area
+     */
+    TruckData[] getTrucks(double latitude, double longitude, long distanceInMeters);
+}
---
+++ app/src/main/java/com/kirussell/tastytrucks/api/data/TruckData.java
@@ -0,0 +1,7 @@
+package com.kirussell.tastytrucks.api.data;
+
+/**
+ * Created by russellkim on 05/04/16.
+ */
+public class TruckData {
+}
